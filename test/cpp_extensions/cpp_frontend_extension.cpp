c0c86afbb0cf6f80f9f2a55abb85fde80a12dfeb
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-06-20 00:28:03 -0700

    Add LOOOK PYBIND11_INTERNALS_H_TAG

--- test/cpp_extensions/cpp_frontend_extension.cpp
+++ test/cpp_extensions/cpp_frontend_extension.cpp
@@ -44,6 +44,7 @@ struct Net : torch::nn::Cloneable<Net> {
 };
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+  fflush(stderr); printf("\nLOOOK Net PYBIND11_INTERNALS_H_TAG=%s %s:%d\n", PYBIND11_INTERNALS_H_TAG, __FILE__, __LINE__); fflush(stdout);
   fflush(stderr); printf("\nLOOOK Net PYBIND11_INTERNALS_ID=%s %s:%d\n", PYBIND11_INTERNALS_ID, __FILE__, __LINE__); fflush(stdout);
   torch::python::bind_module<Net>(m, "Net")
       .def(py::init<int64_t, int64_t>())
